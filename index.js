6c8be34c04ef856e6e108cf5e618f803244575df
Giovanni <g.buono@esis-italia.com>
2026-04-01 12:23:25 +0200

    adding animation + js

--- index.js
+++ index.js
@@ -2,11 +2,7 @@
 const btn_link = document.getElementById('link-click');
 const list_show = document.getElementById('list-hidden');
 
+
 btn_link.addEventListener('click', function () {
-    if(list_show.style.display === 'none') {
-        console.log('ciao');
-        list_show.style.display = 'block';
-    } else {
-        list_show.style.display = 'none';
-    }
+    list_show.classList.toggle('open');
 });
