2f7f7a495b64029980cc83920f0945cbd867e383
Giovanni <gbuono814@gmail.com>
2026-04-02 17:21:45 +0200

    adding the animation js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
 const btn_link = document.getElementById('link-click');
 const list_show = document.getElementById('list-hidden');
-
-
+const user_interface = document.getElementById('user-profile-id')
+const appear_ul = document.getElementById('appear-ul')
 btn_link.addEventListener('click', function () {
     list_show.classList.toggle('open');
+});
+
+
+user_interface.addEventListener('click', function () {
+    appear_ul.classList.toggle('open');
 });
