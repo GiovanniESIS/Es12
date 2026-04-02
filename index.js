b7f2256fa15f4c263c6ead9e2eccefbccecfcd82
Andrei Ceban <a.ceban@esis-italia.com>
2026-04-02 17:23:27 +0200

    conmflict test

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
-const btn_link = document.getElementById('link-click');
-const list_show = document.getElementById('list-hidden');
 const user_interface = document.getElementById('user-profile-id')
 const appear_ul = document.getElementById('appear-ul')
-btn_link.addEventListener('click', function () {
-    list_show.classList.toggle('open');
-});
-
 
 user_interface.addEventListener('click', function () {
-    appear_ul.classList.toggle('open');
-});
+  appear_ul.classList.toggle('open')
+})
+
+
+document
+  .querySelector('aside nav  ul li.fixed')
+  .addEventListener('click', function (link) {
+    this.classList.toggle('open')
+  })
