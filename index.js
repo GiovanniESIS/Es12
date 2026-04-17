cec5872090e8e7d30340e282fe54e8d8ecab03e4
Giovanni <gbuono814@gmail.com>
2026-04-17 15:47:01 +0200

    Improving JS and HTML

--- index.js
+++ index.js
@@ -1,14 +1,144 @@
 const user_interface = document.getElementById('user-profile-id')
 const appear_ul = document.getElementById('appear-ul')
+const btnSee = document.getElementById('btn-card-actions')
+const userContainer = document.getElementById('client-list-id')
+const tableContainer = document.getElementById('bodyTable')
+const tableContainer2 = document.getElementById('bodyTable2')
+let maxUser = 10
+let show = false
+function userData(maxUser){
+    fetch(`https://dummyjson.com/users?limit=${maxUser}`).then(res => res.json()).then((userResponse) => {
 
-user_interface.addEventListener('click', function (el) {
-    console.log(el,this)
-    appear_ul.classList.toggle('open')
+  console.log(userResponse.users)
+  const roles = [];
+  userResponse.users.forEach(item => {
+    
+    const rowDiv = document.createElement('div')
+    const divAvatar = document.createElement('div')
+    const spanPrice = document.createElement('span')
+    const divInfo = document.createElement('div')
+    const divPrice = document.createElement('div')
+
+    
+    rowDiv.classList = 'row-list'
+    divAvatar.classList = 'client-avatar'
+    divAvatar.innerHTML = `<img src="${item.image}" alt="Client Avatar">`
+    rowDiv.appendChild(divAvatar)
+    divInfo.classList = 'client-info'
+    divInfo.innerHTML = `<h5>${item.firstName + " " + item.lastName}</h5> <h6>${item.company.title}</h6>`
+    rowDiv.appendChild(divInfo)
+
+    divPrice.classList = 'client-price'
+    spanPrice.textContent = '€ 1.200'
+    divPrice.appendChild(spanPrice)
+
+    rowDiv.appendChild(divPrice)
+    if (!roles.includes(item.role)) {
+      roles.push(item.role)
+    }
+    userContainer.appendChild(rowDiv)
+  });
+  
+  console.log(roles)
+});
+}
+
+userData(maxUser)
+
+btnSee.addEventListener('click', function(){
+    if(show){
+      maxUser = 10;
+    } else {
+      maxUser = 40;
+    }
+    show = !show
+    console.log(show)
+    userContainer.innerHTML = ''
+    userData(maxUser)
 })
 
+user_interface.addEventListener('click', function (el) {
+  console.log(el, this)
+  
+  appear_ul.classList.toggle('open')
+})
 
+const carts = fetch('https://dummyjson.com/carts?limit=2').then(res => res.json()).then((cartsResponse) => {
+      cartsResponse.carts.forEach(carts => {
+        const tr = document.createElement('tr')
+        const tdTotalProduct = document.createElement('td')
+        const tdQuantitàProduct = document.createElement('td')
+        const tdTotalePrezzo = document.createElement('td')
+        tdTotalProduct.textContent = carts.totalProducts;
+        tdQuantitàProduct.textContent = carts.totalQuantity;
+        tdTotalePrezzo.textContent = "€ " + Math.round(carts.total);
+        tr.appendChild(tdTotalProduct)
+        tr.appendChild(tdQuantitàProduct)
+        tr.appendChild(tdTotalePrezzo)
+        tableContainer2.appendChild(tr)
+        carts.products.forEach(item => {
+
+        const tr = document.createElement('tr')
+        const tdId = document.createElement('td')
+        const tdNomeProdotto = document.createElement('td')
+        const tdQuantita = document.createElement('td')
+        const tdTotale = document.createElement('td')
+        tdId.textContent = item.id;
+        tdNomeProdotto.innerHTML = `<img style=display:flex; width=50px src="${item.thumbnail}" alt="Products Avatar"> ${item.title}`;
+        tdQuantita.textContent = item.quantity;
+        tdTotale.textContent = "€ " + Math.round(item.total);
+        tr.appendChild(tdId)
+        tr.appendChild(tdNomeProdotto)
+        tr.appendChild(tdQuantita)
+        tr.appendChild(tdTotale)
+        tableContainer.appendChild(tr)
+        })
+      });
+      
+})
 document
   .querySelector('aside nav  ul li.fixed')
   .addEventListener('click', function (link) {
     this.classList.toggle('open')
   })
+/*
+<tr class="table-row">
+                                <td class="col nome">Piattaforma Ecommerce</td>
+                                <td class="col cliente">Luca Bianchi</td>
+                                <td class="col-stato"><span class="badge in-progress">In corso</span></td>
+
+                            </tr>
+
+
+
+
+ <tr>
+                            <td>F0123</td>
+                            <td>Tech Solutione</td>
+                            <td class="col-stato"><span class="badge success">Pagata</span></td>
+                            <td>€ 3.500</td>
+                        </tr>
+
+
+
+
+
+
+
+
+
+
+
+  <div class="row-list">
+                            <div class="client-avatar">
+                                <img src="assets/avatars/1.png" alt="Client Avatar">
+                            </div>
+                            <div class="client-info">
+                                <h5>Luca Bianchi</h5>
+                                <h6>TechSolution</h6>
+                            </div>
+                            <div class="client-price">
+                                <span>€ 1.200</span>
+                            </div>
+  </div>
+*/
